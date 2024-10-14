6b11b39db5a8a7ad512ab72e4bdc9c25537856a2
Callahan <callahancarvalho@gmail.com>
2024-10-14 21:23:42 +0530

    deployed contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,3 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.27",
 };
+
+// require("@nomiclabs/hardhat-waffle");
+
+// module.exports = {
+//     solidity: "0.8.0", // Specify the compiler version
+// };
+
